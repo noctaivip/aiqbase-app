97cac4557f344bb37cd9b511f5fa8536cf12399b
NOCTAI - Private Capital Systems <noctai.vip@proton.me>
2026-08-10 03:43:26 +0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'aiqbase-vocab-500-v1';
+const CACHE_NAME = 'aiqbase-vocab-500-v2';
 const APP_SHELL = [
   './',
   './index.html',
   './app_preview.html',
   './manifest.webmanifest',
   './content/questions.json',
-  './content/vocabulary.json',
+  './content/person-types.json',
   './icons/icon-192.png',
   './icons/icon-512.png'
 ];
